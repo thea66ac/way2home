781156a264da18e20f73e36b19d6a943ac0b25b8
A66ac <thea66ac@gmail.com>
2021-07-17 23:01:38 +0200

    added start files for project

---
+++ src/main/java/com/webapp/way2home/Way2homeApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.way2home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Way2homeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Way2homeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webapp/way2home/Way2homeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.way2home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Way2homeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
